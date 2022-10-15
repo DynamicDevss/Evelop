499114281327637d1cd066b757989cf6eed9782a
Cisa <cianaparicio@gmail.com>
2022-10-16 00:22:40 +0200

    oh my god i almost f'ed up

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run("OTAxODE5NTU1NDMxNzM5NDIz.YXVa6Q.tYI3hfVU83ep0bYqUdQXUBHGii4")
+client.run("we're not dumb")
